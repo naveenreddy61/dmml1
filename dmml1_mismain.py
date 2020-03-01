9504848d877be51c4dc5a5af5b5d3137c577167d
naveenreddy61 <naveenreddy61@gmail.com>
2020-03-01 20:09:09 +0530

    finished coding. need documentation and automation and abstraction

--- dmml1_mismain.py
+++ dmml1_mismain.py
@@ -1,23 +1,52 @@
 from dmml1_mis import *
 
+
+def numbersToWords(answer, vocabulary, outputfile):
+    '''
+    INPUT:  answer will be frequent itemsets of form list of lists
+            ex-[[1,2,3],[(1,3),(2,4)],[(2,3,4)]]
+            vocabulary is dict of form {1:'a',2:'the'}
+            outputfile is string which is where the output will be stored
+    OUTPUT: out file will be opened with name outputfile and each number will be converted,
+            to corresponding word which is stored vocabulary dictionary
+    '''
+    out = open(outputfile, "w")
+    for i in range(0, len(answer)):
+        if i == 0:
+            # itemsets of size 1 is treated as a special case
+            F = []
+            for _,element1 in answer[0]:
+                F.append(vocabulary[element1])
+            out.write(str(F)+'\n')
+        else:
+            # take each tuple, get the words associated with each number inside the tuple,
+            # check vocabulary and create a new list
+            F = []
+            for klist in answer[i]:
+                F.append(list((vocabulary[element]
+                     for _,element in klist)))
+            out.write(str(F)+'\n')
+    out.close()
+    return
+
+'''
 LS = 0.08
 beta = 0.8
-K=5
-'''
+K = 5
 test_transactions,test_vocabulary, test_docs_of_words, test_count_of_words =readtransactions('docword.test.txt','vocab.test.txt')
 num_test_transactions = len(test_transactions)
-#print(MSapriori(test_transactions,test_docs_of_words,test_count_of_words,LS,beta,K))
-#print(test_count_of_words)
-#print(getMIS(test_docs_of_words,LS,beta,num_test_transactions))
+answer = MSapriori(test_transactions,test_docs_of_words,test_count_of_words,LS,beta,K)
+numbersToWords(answer, test_vocabulary, "test_answer.txt")
+'''
 
+LS = 0.09
+beta = 0.9
+K = 10
 kos_transactions, kos_vocabulary, kos_docs_of_words, kos_count_of_words = readtransactions('docword.kos.txt','vocab.kos.txt')
 num_kos_transactions = len(kos_transactions)
-#print(getMIS(kos_docs_of_words,LS,beta,num_kos_transactions))
-outputfile = open("kos_mis_answer.txt","w")
-for fk in MSapriori(kos_transactions,kos_docs_of_words,kos_count_of_words,LS,beta,K):
-    outputfile.write(str(fk)+'\n')
-outputfile.close()
-'''
+answer = MSapriori(kos_transactions,kos_docs_of_words,kos_count_of_words,LS,beta,K)
+numbersToWords(answer, kos_vocabulary, "kos_word_mis_answer.txt")
+
 '''
 LS = 0.08
 beta = 0.8
@@ -29,14 +58,18 @@ outputfile = open("enron_mis_answer.txt","w")
 for fk in MSapriori(enron_transactions,enron_docs_of_words,enron_count_of_words,LS,beta,K):
     outputfile.write(str(fk)+'\n')
 outputfile.close()
+'''
+
 '''
 LS = 0.04
 beta = 0.8
-K=5
-enron_transactions, enron_vocabulary, enron_docs_of_words, enron_count_of_words = readtransactions('docword.enron.txt','vocab.enron.txt')
+K = 5
+enron_transactions, enron_vocabulary, enron_docs_of_words, enron_count_of_words = readtransactions(
+    'docword.enron.txt', 'vocab.enron.txt')
 num_enron_transactions = len(enron_transactions)
-#print(getMIS(kos_docs_of_words,LS,beta,num_kos_transactions))
-outputfile = open("enron_mis_answer.txt","w")
-for fk in MSapriori(enron_transactions,enron_docs_of_words,enron_count_of_words,LS,beta,K):
+# print(getMIS(kos_docs_of_words,LS,beta,num_kos_transactions))
+outputfile = open("enron_mis_answer.txt", "w")
+for fk in MSapriori(enron_transactions, enron_docs_of_words, enron_count_of_words, LS, beta, K):
     outputfile.write(str(fk)+'\n')
 outputfile.close()
+'''
